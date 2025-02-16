8055297094a388f2726d69dbe2ee8ed3a69b9057
mj-stack <mjoshi1819@gmail.com>
2025-02-16 13:38:13 +0530

    Props

--- Website/my-react-app/src/App.jsx
+++ Website/my-react-app/src/App.jsx
@@ -1,10 +1,14 @@
-import Button from "./Buttons";
+import Student from "./Students";
 
 function App() {
 
   return(
     <>
-      <Button/>
+      <Student name="Mukul Joshi" age={21} isStudent={true}/>
+      <Student name="Anmol Bisht" age={21} isStudent={true}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student />
     </>
   );
 }
---
+++ Website/my-react-app/src/Students.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'
+
+function Student(props) {
+    return(
+        <div className="student">
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+        </div>
+    );
+}
+
+export default Student;

ce5e0a45b3e88c3cc836fabbaf07a1da55a0bbd6
Mukul Joshi <mjoshi1819@gmail.com>
2025-03-16 23:14:04 +0530

    Updated

--- react-course-2/projects/clock-v1/src/App.jsx
+++ react-course-2/projects/clock-v1/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Clock from './components/Clock'
 import Heading from './components/heading'
 import Moto from './components/Moto'

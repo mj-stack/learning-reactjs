f50ce6bac1a1dddf546f2a60a7965552d00f9d4e
Mukul Joshi <mjoshi1819@gmail.com>
2025-03-19 22:00:30 +0530

    Use reducer

--- react-course-2/first-react-app/projects/todo-v3/src/App.jsx
+++ react-course-2/first-react-app/projects/todo-v3/src/App.jsx
@@ -1,38 +1,21 @@
+import "./App.css"
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
 import TodoItems from "./components/TodoItems";
-import "./App.css"
-import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
-import { TodoItemsContext } from "./store/todo-item-store";
+import TodoItemsContextProvider from "./store/todo-item-store";
 
 function App() {
-  
-  const [todoItems, setTodoItems] = useState([])
-
-  const addNewItem = (itemName, itemDueDate) => {
-    setTodoItems((currentVal) => [...currentVal, {name: itemName, dueDate: itemDueDate}] )
-  }
-
-  const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter(item => item.name !== todoItemName)
-    setTodoItems(newTodoItems)
-  }
 
   return (
-    <TodoItemsContext.Provider 
-      value={{
-        todoItems,
-        addNewItem,
-        deleteItem
-      }} >
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
         <AddTodo />
         <WelcomeMessage />
         <TodoItems />
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 

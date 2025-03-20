d7d09531e2437a70ade4a06303ed137946e3c6a9
Mukul Joshi <mjoshi1819@gmail.com>
2025-03-20 16:57:22 +0530

    Handling loading state

--- react-course-2/first-react-app/projects/7-social-media-v1/src/components/PostList.jsx
+++ react-course-2/first-react-app/projects/7-social-media-v1/src/components/PostList.jsx
@@ -1,26 +1,33 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import Post from "./Post"
 import {PostList as PostListData} from "../store/post-list-store"
 import WelcomMessage from "./WelcomeMessage"
+import LoadingSpinner from "./LoadingSpinner"
 
 const PostList = () => {
   const {postList, addInitialPosts} = useContext(PostListData)
+  const [fetching, setFetching] = useState(false)
 
   useEffect(() => {
+    setFetching(true)
     fetch('https://dummyjson.com/posts')
     .then(res => res.json())
     .then(data => {
       addInitialPosts(data.posts)
+      setFetching(false)
     })
   }, [])
 
   return(
     <>
     {
-      postList.length === 0 && <WelcomMessage />
+      fetching && <LoadingSpinner />
     }
     {
-      postList.map((post) => (
+      !fetching && postList.length === 0 && <WelcomMessage />
+    }
+    {
+      !fetching && postList.map((post) => (
       <Post key={post.id} post={post} />
       ))
     }

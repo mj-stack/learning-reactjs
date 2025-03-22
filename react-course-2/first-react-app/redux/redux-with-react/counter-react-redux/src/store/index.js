b9165238aa200c76adf150a8ec3bc441ee984761
Mukul Joshi <mjoshi1819@gmail.com>
2025-03-22 16:55:39 +0530

    Add initial setup for counter app with Redux, including components and configuration files

---
+++ react-course-2/first-react-app/redux/redux-with-react/counter-react-redux/src/store/index.js
@@ -0,0 +1,20 @@
+import {createStore} from 'redux'
+
+const INTIAL_VALUE = {
+  counter: 0
+}
+
+const counterReducer = (store = INTIAL_VALUE, action) => {
+  
+  if(action.type === "INCREMENT") {
+    return {counter: store.counter + 1}
+  } else if(action.type === "DECREMENT") {
+    return {counter: store.counter - 1}
+  }
+
+  return store
+}
+
+const counterStore = createStore(counterReducer)
+
+export default counterStore
